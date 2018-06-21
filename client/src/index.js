d76e75caf8b6d133e98fafb39bc2f5fd1b2db573
Josh <jhint2424@gmail.com>
2018-06-21 04:02:09 -0700

    import

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
